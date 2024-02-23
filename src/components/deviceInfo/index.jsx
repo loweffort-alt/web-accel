68dbc87cc774b961cbee7542a316322380267955
loweffort-alt <farfan_alexander@outlook.com>
2024-02-23 18:48:19 -0500

    New feature: TailwindCSS

--- src/components/deviceInfo/index.jsx
+++ src/components/deviceInfo/index.jsx
@@ -1,7 +1,9 @@
 export const DeviceInfo = () => {
   return (
     <>
-      <h1>Centro de monitoreo de estaciones sísmicas</h1>
+      <h1 className="text-red-200">
+        Centro de monitoreo de estaciones sísmicas
+      </h1>
 
       <h2>1. Información sobre la estación sísmica</h2>
       <p>Grupo: xxxxxxx</p>
