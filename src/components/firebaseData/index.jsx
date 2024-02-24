98816e3c4e12c6a6bfdcdef8f4fb1ebfd83bc313
loweffort-alt <farfan_alexander@outlook.com>
2024-02-24 04:16:34 -0500

    New Features: Accuracy Chart added

--- src/components/firebaseData/index.jsx
+++ src/components/firebaseData/index.jsx
@@ -3,20 +3,29 @@ import { fetchDataFromFirebase } from "../../fetchData/fetchDataFromFirebase";
 import { LineChart } from "../../components/lineChart";
 
 export const FirebaseData = () => {
+  const [showData, setShowData] = useState(false);
   const [sensorData, setSensorData] = useState([]);
   const [maxValues, setMaxValues] = useState([]);
   const [startTime, setStartTime] = useState(null);
+  const [diffTime, setDiffTime] = useState([]);
   const [graphData, setGraphData] = useState({
+    labels: [],
+    datasets: [{}],
+  });
+  const [grahpAccuracy, setGraphAccuracy] = useState({
     labels: [],
     datasets: [{}],
   });
 
   useEffect(() => {
-    fetchDataFromFirebase().then(({ sensorData, maxValues, startTime }) => {
-      setSensorData(sensorData);
-      setMaxValues(maxValues);
-      setStartTime(startTime);
-    });
+    fetchDataFromFirebase().then(
+      ({ sensorData, maxValues, startTime, difMilSec }) => {
+        setSensorData(sensorData);
+        setMaxValues(maxValues);
+        setStartTime(startTime);
+        setDiffTime(difMilSec);
+      },
+    );
   }, []);
 
   useEffect(() => {
@@ -46,6 +55,19 @@ export const FirebaseData = () => {
         },
       ],
     });
+    setGraphAccuracy({
+      labels: sensorData.map((e) => e.numberData),
+      datasets: [
+        {
+          label: "Diferencia de tiempo entre cada dato",
+          data: diffTime,
+          borderColor: "#34a853",
+          pointBackgroundColor: "#34a853",
+          pointRadius: 2,
+          showLine: false,
+        },
+      ],
+    });
   }, [sensorData]);
 
   return (
@@ -62,34 +84,58 @@ export const FirebaseData = () => {
         </p>
       </div>
 
-      <div className="rounded-xl p-3 flex flex-col gap-2 items-center m-auto">
-        <h2 className="text-lg font-bold text-center">Datos del registro</h2>
-
-        <div className="w-[1000px] h-full bg-white m-auto p-5">
-          <LineChart chartData={graphData} />
+      <div className="p-3 flex flex-col gap-10 items-center m-auto">
+        <div>
+          <h2 className="text-lg font-bold text-center mb-5">
+            Datos del registro
+          </h2>
+          <div className="w-[1000px] h-full bg-white m-auto p-5 rounded-3xl">
+            <LineChart chartData={graphData} />
+          </div>
         </div>
 
-        <div className="olas hidden">
-          <table>
-            <thead>
-              <tr>
-                <th>Eje X</th>
-                <th>Eje Y</th>
-                <th>Eje Z</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sensorData.map((entry, index) => (
-                <tr key={index}>
-                  <td>{entry.dataX}</td>
-                  <td>{entry.dataY}</td>
-                  <td>{entry.dataZ}</td>
-                  <td>{entry.currentTime}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div>
+          <h2 className="text-lg font-bold text-center mb-5">
+            Precisión del registro
+          </h2>
+          <div className="w-[1000px] h-full bg-white m-auto p-5 rounded-3xl">
+            <LineChart chartData={grahpAccuracy} />
+          </div>
+        </div>
+
+        <div className="mt-5 flex flex-col">
+          <button
+            type="button"
+            className="mb-10 text-white hover:text-white border border-white hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            onClick={() => setShowData(!showData)}
+          >
+            Mostrar Datos
+          </button>
+
+          {showData && (
+            <div>
+              <table className="border-separate border-spacing-x-10 border-spacing-y-3 border border-white rounded-lg p-5 text-center">
+                <thead>
+                  <tr>
+                    <th className="border-2 border-red-500">Eje X</th>
+                    <th className="border-2 border-red-500">Eje Y</th>
+                    <th className="border-2 border-red-500">Eje Z</th>
+                    <th className="border-2 border-red-500">Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sensorData.map((entry, index) => (
+                    <tr key={index}>
+                      <td>{entry.dataX}</td>
+                      <td>{entry.dataY}</td>
+                      <td>{entry.dataZ}</td>
+                      <td>{entry.currentTime}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       </div>
     </>
--- src/fetchData/fetchDataFromFirebase.js
+++ src/fetchData/fetchDataFromFirebase.js
@@ -3,22 +3,43 @@ import { getDatabase, ref, child, get } from "firebase/database";
 
 const databaseRef = ref(getDatabase(app));
 
+function calcularDiferenciasDeTiempoEnMilisegundos(arrayDeTiempos) {
+  const tiemposEnDate = arrayDeTiempos.map((tiempo) => {
+    const [horas, minutos, segundos, milisegundos] = tiempo
+      .split(/[:.]/)
+      .map(Number);
+    return new Date(0, 0, 0, horas, minutos, segundos, milisegundos);
+  });
+
+  const diferenciasEnMilisegundos = [];
+  for (let i = 1; i < tiemposEnDate.length; i++) {
+    const diferenciaEnMilisegundos = tiemposEnDate[i] - tiemposEnDate[i - 1];
+    diferenciasEnMilisegundos.push(diferenciaEnMilisegundos / 1000);
+  }
+
+  return diferenciasEnMilisegundos;
+}
+
 export async function fetchDataFromFirebase() {
   const snapshot = await get(child(databaseRef, "data"));
   if (snapshot.exists()) {
     const rawData = snapshot.val();
-    const filteredData = Object.values(rawData).map((el) => ({
+    const filteredData = Object.values(rawData).map((el, i) => ({
       dataX: parseFloat(el.dataX),
       dataY: parseFloat(el.dataY),
       dataZ: parseFloat(el.dataZ),
       currentTime: el.currentTime,
+      numberData: i,
     }));
     const maxValues = ["dataX", "dataY", "dataZ"].map((axis) =>
       Math.max(...filteredData.map((entry) => Math.abs(entry[axis]))),
     );
+    const arrayMiliseconds = filteredData.map((el) => el.currentTime);
+    const difMilSec =
+      calcularDiferenciasDeTiempoEnMilisegundos(arrayMiliseconds);
     const startTime =
       filteredData.length > 0 ? filteredData[0].currentTime : null;
-    return { sensorData: filteredData, maxValues, startTime };
+    return { sensorData: filteredData, maxValues, startTime, difMilSec };
   } else {
     throw new Error("No data available");
   }
